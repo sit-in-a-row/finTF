21873a58dbf3a038032b8d1415fac7c787bef948
김지원 <127467108+gamjawon@users.noreply.github.com>
2025-01-17 20:30:01 +0900

    t1 code

--- get_data/get_data_raw.py
+++ get_data/get_data_raw.py
@@ -1,7 +1,7 @@
 from raw import update_all_raw_info
 
 # 전체 업데이트 코드
-start_date = '20240630'
-end_date = '20240701'
+start_date = '20241001'
+end_date = '20241201'
 
 update_all_raw_info(start_date, end_date)
